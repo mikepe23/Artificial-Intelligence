65874d614144a5f85a45be0c7efb7050009018e2
Luke Rooney <ltrooney@berkeley.edu>
2019-02-07 11:42:49 -0800

    crafted the magic corners heuristic

--- searchAgents.py
+++ searchAgents.py
@@ -358,6 +358,17 @@ class CornersProblem(search.SearchProblem):
             if self.walls[x][y]: return 999999
         return len(actions)
 
+def manSums(location, remaining_locs):
+    if not remaining_locs:
+        return 0
+    sums = []
+    for corner in remaining_locs:
+        rem_copy = list(remaining_locs).copy()
+        rem_copy.remove(corner)
+        path_distance = util.manhattanDistance(location, corner) + manSums(corner, rem_copy)
+        sums.append(path_distance)
+    #print(sums)
+    return min(sums)
 
 def cornersHeuristic(state, problem):
     """
@@ -375,11 +386,9 @@ def cornersHeuristic(state, problem):
     corners = problem.corners # These are the corner coordinates
     walls = problem.walls # These are the walls of the maze, as a Grid (game.py)
 
-    man_sum = sum([util.manhattanDistance(state[0], corner) for corner in state[1]])
-    if len(state[1]) > 1:
-        return man_sum/2
-    else:
-        return man_sum
+    state_loc, state_corners_left = state
+    
+    return manSums(state_loc, state_corners_left)
 
 class AStarCornersAgent(SearchAgent):
     "A SearchAgent for FoodSearchProblem using A* and your foodHeuristic"
